479583161e7e85c7261e30c19b1e364cfa36ff8f
anuragsingh23 <anuragshekhawat17@gmail.com>
2021-10-24 17:10:22 +0530

    Navigation component Example

---
+++ app/src/main/java/com/example/navdemo/VerifyDetailsFragment.kt
@@ -0,0 +1,40 @@
+package com.example.navdemo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+
+class VerifyDetailsFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+
+        val name =arguments?.getString("name")
+        val mobilenumber=arguments?.getLong("mobile")
+
+
+        val rootView=inflater.inflate(R.layout.fragment_verify_details, container, false)
+
+        val tvName=rootView.findViewById<TextView>(R.id.tv_name)
+        val tvMobileNumber=rootView.findViewById<TextView>(R.id.tv_mobile_number)
+
+        tvName.text=name
+        tvMobileNumber.text=mobilenumber.toString()
+
+
+        return rootView
+    }
+}
